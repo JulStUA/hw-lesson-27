d9e9d0f3f6b64a9d6e4bedf9aab324c6eaf76d2e
JulStP <julkagavgav9@gmail.com>
2023-07-15 03:33:47 +0300

    Add second.txt

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const fs = require('node:fs/promises');
 
+fs.readFile('./first.txt', 'utf-8')
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
 
+fs.writeFile('second.txt', 'I love NodeJS');
+ 
