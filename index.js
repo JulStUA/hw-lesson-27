cec04202b57cb08f37097431fdc8086e5ddc0ade
JulStP <julkagavgav9@gmail.com>
2023-07-15 03:43:57 +0300

    Add first.txt in the second.txt

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ fs.writeFile('second.txt', 'I love NodeJS');
 fs.readFile('./second.txt', 'utf-8')
   .then(res => console.log(res))
   .catch(err => console.log(err));
-  
+ 
+fs.readFile ('./first.txt', 'utf-8')
+  .then((res) => fs.appendFile('second.txt','\n'+ res))
+  .catch(err => console.log(err));
